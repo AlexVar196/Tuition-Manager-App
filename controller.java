dc6929162e83289684b9bd15c067a9f3fa8ef6eb
Michael Flores <michael.flores.098@gmail.com>
2020-03-04 18:58:00 -0500

    Add files via upload

---
+++ controller.java
@@ -0,0 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class controller {
+
+    @FXML
+    private Button theButton;
+
+    @FXML
+    private Label outputLabel;
+
+    @FXML
+    private Label promptLabel;
+
+    @FXML TextField theTextField;
+}
---
+++ main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application{
+
+    public void start(Stage primaryStage) throws Exception {
+
+        //Load the FXML file.
+        Parent root = FXMLLoader.load(getClass().getResource("projectThree.fxml"));
+
+        primaryStage.setTitle("Program 3 - Tuition Manager");
+        primaryStage.setScene(new Scene(root, 600, 525));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+
+        //Launch the application.
+        launch(args);
+    }
+}
